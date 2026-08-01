b6d330eea5059345538e20e396a308180aa059a2
lizaveta2193-dev <lizaveta2193@gmail.com>
2026-08-01 23:59:33 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE="cao-3000-v4";
+const CACHE="cao-corrected-v5";
 const FILES=["./","./index.html","./styles.css","./app.js","./course-data.js","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
